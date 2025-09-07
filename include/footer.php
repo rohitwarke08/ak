cc5672dc8eb9068033a05965460e2ac772fe75c7
Rohit Warke <115525385+rohitwarke08@users.noreply.github.com>
2025-09-08 02:03:25 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,33 @@
+<div class="footer-wrapper py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <div>
+                    <img src="images/logo.svg" alt="" srcset="">
+                </div>
+                <div>
+                    <ul class="d-flex justify-content-center pt-5 m-0 px-0">
+            <li>
+              <a class="nav-link text-white"href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#expertise">Expertise</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#work">Work</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#experience">Experience</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#skill">Skill</a>
+            </li>
+            <li>
+              <a class="nav-link text-white" href="#contact">Contact</a>
+            </li>
+          </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ include/header.php
@@ -0,0 +1,41 @@
+<!-- Navbar section  -->
+  <header class="header_wrapper">
+    <nav class="navbar navbar-expand-lg fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="images/logo.svg" alt="logo" class="img-fluid">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <!-- <span class="navbar-toggler-icon"></span> -->
+          <i class="fas fa-stream navbar-toggler-icon"></i>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul class="navbar-nav menu-navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#expertise">Expertise</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#work">Work</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#experience">Experience</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#skill">Skill</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item mt-3 mt-lg-0">
+              <a class="main-btn-nav" href="#contact">Hire me</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <!-- Navbar section end -->
